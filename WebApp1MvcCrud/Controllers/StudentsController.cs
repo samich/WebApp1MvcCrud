c06e8ee9d7f1f7c3dc483c7a5cbc78c9c0b48a81
samich <itssami@gmail.com>
2023-06-06 22:49:27 -0400

    Student controller

---
+++ WebApp1MvcCrud/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp1MvcCrud.Data;
+using WebApp1MvcCrud.Models;
+
+namespace WebApp1MvcCrud.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        private StudentDbContext _context;
+
+        public StudentsController(StudentDbContext studentDbContext) 
+        { 
+            _context = studentDbContext;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Student> students = _context.Students.ToList();
+
+            return View(students);
+        }
+    }
+}
