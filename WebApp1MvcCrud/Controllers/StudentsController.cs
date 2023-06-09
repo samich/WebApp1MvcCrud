a1ff4a61c6a4ccd141a90504c0be326458a83b9b
samich <itssami@gmail.com>
2023-06-09 18:04:05 -0400

    Edit action and view

--- WebApp1MvcCrud/Controllers/StudentsController.cs
+++ WebApp1MvcCrud/Controllers/StudentsController.cs
@@ -51,5 +51,63 @@ namespace WebApp1MvcCrud.Controllers
 
             return View("Add");
         }
+
+        [HttpGet]
+        public IActionResult Edit(Guid Id) 
+        {
+            var student = _context.Students.FirstOrDefault(x => x.Id == Id);
+
+            if (student != null) {
+
+                var editStudentRequest = new EditStudentRequest
+                {
+
+                    Id = student.Id,
+                    FirstName = student.FirstName,
+                    LastName = student.LastName,
+                    Age = student.Age,
+                    Grade = student.Grade
+                };
+
+                return View(editStudentRequest);
+            }
+            
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditStudentRequest request)
+        {
+
+            Student student = new Student
+            {
+                Id  = request.Id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Age = request.Age,
+                Grade = request.Grade
+            };
+
+            //fetch existing row from database
+            Student? existingStudent = _context.Students.Find(request.Id);
+
+            if (existingStudent != null) {
+
+                existingStudent.FirstName = student.FirstName; 
+                existingStudent.LastName = student.LastName;
+                existingStudent.Age = student.Age;
+                existingStudent.Grade = student.Grade;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            
+            //if there is no record in db, display error            
+            return View("Edit", new { Id = request.Id });
+        }
     }
+
+    
 }
---
+++ WebApp1MvcCrud/Models/ViewModels/EditStudentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp1MvcCrud.Models.ViewModels
+{
+    public class EditStudentRequest
+    {
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public short Age { get; set; }
+        public float Grade { get; set; }
+    }
+}
